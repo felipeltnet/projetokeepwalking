003ff92248cdcc4dfd13b73e36a8e20c07bb7f01
felipeltnet <felipeltnet@hotmail.com>
2015-11-12 15:44:55 -0200

    Adicionar Classes de categoria

---
+++ Negocio/Categoria.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Persistencia;
+
+namespace Negocio
+{
+    public class Categoria
+    {
+        String nomeCategoria;
+
+        public void adicionar(String nomeCategoria)
+        {
+            pCategoria objCategoria = new pCategoria();
+            objCategoria.adicionar(nomeCategoria);
+        }
+        public void deletar()
+        {
+        }
+        public void alterar()
+        {
+        }
+        public String consultar()
+        {
+            return "";
+        }
+    }
+}
