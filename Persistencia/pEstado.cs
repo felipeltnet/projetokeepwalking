acc8e160262a5173a96b29127be6726e3260a56f
apollodasilvalemos <apollodasilvalemos@hotmail.com>
2015-11-12 15:11:57 -0200

    Persistência e negócio Estado

---
+++ Persistencia/pEstado.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia
+{
+     public class pEstado
+    {
+
+        String estado;
+
+        public void adicionar(String estado)
+        {
+            String SQL = "INSERT INTO Estado(idEstado, idRegiao, estado) VALUES(1,1 '" + estado + "')";
+            Conexao oConexao = new Conexao("SQLServer");
+            oConexao.executeNoQuery(SQL);
+            oConexao.fechaConexao();
+        }
+        public void alterar()
+        {
+
+        }
+        public void deletar()
+        {
+
+        }
+        public String consultar()
+        {
+            return "";
+        }
+    }
+}
