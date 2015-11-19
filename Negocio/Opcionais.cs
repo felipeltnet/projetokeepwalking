7bf79ac5afaa89cde44bd3efe58de8109b5985e9
schumacherfernandes <schumacherfernandes@gmail.com>
2015-11-19 15:51:42 -0200

    Arrumar Patrocinador e add Opcionais

---
+++ Negocio/Opcionais.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Persistencia;
+using System.Data;
+
+namespace Negocio
+{
+   public class Opcionais
+    {        
+       String nomeOpcionais;
+
+        public void adicionar(String nomeOpcionais)
+        {
+            pOpcionais objOpcionais = new pOpcionais();
+            objOpcionais.adicionar(nomeOpcionais);
+        }
+        public void alterar()
+        {
+
+        }
+        public void deletar()
+        {
+
+        }
+        public String consultar()
+        {
+            return "";
+        }
+    }
+}
+
