af30bd39ba037166b66be613ec94808ab7938bab
luizeduardocostadaml <luizeduardocostadaml@gmail.com>
2015-11-19 13:54:41 -0200

    Cadastro de Cores

---
+++ Negocio/Cores.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Persistencia;
+
+namespace Negocio
+{
+    public class Cores
+    {
+        String nomeCor;
+
+        public void adicionar(String nomeCor)
+        {
+            pCores objCores = new pCores();
+            objCores.adicionar(nomeCor);
+        }
+        public void deletar()
+        { }
+        public void alterar()
+        { }
+        public String consultar()
+        {
+            return "";
+        }
+    }
+}
