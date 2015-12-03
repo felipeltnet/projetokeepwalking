a8494f82cc088406e137ef60c02426aee40df1a7
luizeduardocostadaml <luizeduardocostadaml@gmail.com>
2015-12-03 15:01:37 -0200

    Alterar,Consultar e Deletar de NewsLetter

--- Persistencia/pNewsLetter.cs
+++ Persistencia/pNewsLetter.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Persistencia
 {
     public class pNewsLetter
     {
         String email;
+        String idEmail;
 
         public void adicionar(String email)
         {
@@ -17,17 +20,32 @@ namespace Persistencia
             oConexao.executeNoQuery(SQL);
             oConexao.fechaConexao();
         }
-        public void alterar()
+        public void alterar(String idEmail)
         {
-
+            String SQL = "UPDATE NewsLetter SET email = '" + email + "' WHERE idNewsLetter = '" + idEmail + "'";
+            Conexao oConexao = new Conexao("SQLServer");
+            oConexao.executeNoQuery(SQL);
+            oConexao.fechaConexao();
         }
-        public void deletar()
+        public void deletar(String idEmail)
         {
-
+            String SQL = "DELETE FROM NewsLetter WHERE idNewsLetter = '" + idEmail + "'";
+            Conexao oConexao = new Conexao("SQLServer");
+            oConexao.executeNoQuery(SQL);
+            oConexao.fechaConexao();
         }
-        public String consultar()
+        public DataSet consultar()
         {
-            return "";
+            String SQL = "SELECT * FROM NewsLetter";
+
+            Conexao objConexao = new Conexao("SQLServer");
+
+            SqlDataAdapter adapter = new SqlDataAdapter(SQL, objConexao.cn);
+            DataSet ds = new DataSet("Tabela");
+            adapter.Fill(ds, "Tabela");
+
+            objConexao.fechaConexao();
+            return ds;
         }
     }
 }
