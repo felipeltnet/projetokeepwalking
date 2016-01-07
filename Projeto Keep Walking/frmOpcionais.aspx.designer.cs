f4331e016955b393b756f73e7ad4ad8993023af4
felipeltnet <felipeltnet@hotmail.com>
2016-01-07 14:14:34 -0200

    Retirar botão consultar Opcionais

--- Projeto Keep Walking/frmOpcionais.aspx.designer.cs
+++ Projeto Keep Walking/frmOpcionais.aspx.designer.cs
@@ -76,15 +76,6 @@ namespace Projeto_Keep_Walking {
         protected global::System.Web.UI.WebControls.Button btnAlterar;
         
         /// <summary>
-        /// btnConsultar control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnConsultar;
-        
-        /// <summary>
         /// gdvOpcionais control.
         /// </summary>
         /// <remarks>
