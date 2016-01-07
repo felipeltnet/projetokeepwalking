bc4052845c218e8bb852ce233c61916d07e84542
mandaaalves <amanda.alves11@gmail.com>
2016-01-07 14:59:43 -0200

    Add Correção de cambio

---
+++ Negocio/Cambio.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Persistencia;
+
+namespace Negocio
+{
+    public class Cambio
+    {
+        String nomeCambio;
+
+        public void adicionar(String nomeCambio)
+        {
+            pCambio objCambio = new pCambio();
+            objCambio.adicionar(nomeCambio);
+        }
+        public void deletar(String idCambio)
+        {
+            pCambio objCambio = new pCambio();
+            objCambio.deletar(idCambio);
+        }
+        public void alterar(String idCambio, String nomeCambio)
+        {
+            pCambio objCambio = new pCambio();
+            objCambio.alterar(idCambio, nomeCambio);
+        }
+        public DataSet consultar()
+        {
+            pCambio objCambio = new pCambio();
+            return objCambio.consultar();
+        }
+    }
+}
