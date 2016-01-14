04c3b216d18c06bad6fe82c01cb9606dba73c391
thiagomem <thiago2008br@hotmail.com>
2016-01-14 15:29:06 -0200

    Enviar NewsLetter

--- Negocio/NewsLetter.cs
+++ Negocio/NewsLetter.cs
@@ -5,11 +5,39 @@ using System.Text;
 using System.Threading.Tasks;
 using Persistencia;
 using System.Data;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Net.Configuration;
+using System.Net;
 
 namespace Negocio
 {
     public class NewsLetter
     {
+
+        String emailorigin;
+        String emaildestino;
+        String titulomsg;
+        String corpomsg;
+
+
+        public void enviarEmail(String emailorigin, String emaildestino, String titulomsg, String corpomsg)
+        {
+            //cria uma mensagem
+            MailMessage mail = new MailMessage();
+
+            //define os endereços
+            mail.From = new MailAddress(emailorigin);
+            mail.To.Add(emaildestino);
+
+            //define o conteúdo
+            mail.Subject = titulomsg;
+            mail.Body = corpomsg;
+
+            //envia a mensagem
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+            smtp.Send(mail);
+        }
 
         public void adicionar(String email)
         {
